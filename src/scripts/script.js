62946ef526a02f377fc5ff8b940fee293073e74c
omario <o.khandxb@gmail.com>
2020-10-17 21:22:37 -0400

    new colours

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,48 @@
 function talk() {
   const data = {
-    text: "on"
+    text: "green"
+  }  
+  
+  fetch('https://cors-anywhere.herokuapp.com/http://egg.hopto.org:8042/light', {
+  method: 'POST', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+}
+
+function khaana() {
+  const data = {
+    text: "red"
+  }  
+  
+  fetch('https://cors-anywhere.herokuapp.com/http://egg.hopto.org:8042/light', {
+  method: 'POST', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+}
+
+function night() {
+  const data = {
+    text: "blue"
   }  
   
   fetch('https://cors-anywhere.herokuapp.com/http://egg.hopto.org:8042/light', {
