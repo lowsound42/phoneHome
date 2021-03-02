a097c6ab781560e85a9ad90ad36b3702f5e3a9bd
Omar Khan <o.khandxb@gmail.com>
2021-03-02 18:22:01 -0500

    test

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,62 +1,62 @@
 function talk() {
-  const data = {
-    text: "green"
-  }  
-  
-  fetch('http://egg.hopto.org:8042/light', {
-  method: 'POST', 
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+    const data = {
+        text: 'green'
+    };
+
+    fetch('http://24.212.130.181:8042/light', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
 
 function khaana() {
-  const data = {
-    text: "red"
-  }  
-  
-  fetch('http://egg.hopto.org:8042/light', {
-  method: 'POST', 
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+    const data = {
+        text: 'red'
+    };
+
+    fetch('http://24.212.130.181:8042/light', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
 
 function night() {
-  const data = {
-    text: "blue"
-  }  
-  
-  fetch('http://egg.hopto.org:8042/light', {
-  method: 'POST', 
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+    const data = {
+        text: 'blue'
+    };
+
+    fetch('http://24.212.130.181:8042/light', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
