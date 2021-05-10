ca96f14edad6e14307606d93b9a51f2841a0f9be
Omar Khan <o.khandxb@gmail.com>
2021-05-10 16:48:50 -0400

    ip added

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,62 +1,44 @@
-function talk() {
-    const data = {
-        text: 'green'
+function httpGetAsync(url, data) {
+    var xmlHttp = new XMLHttpRequest();
+    let ipResult;
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+            ipResult = xmlHttp.responseText;
+        changeLight(ipResult, data);
     };
+    xmlHttp.open('GET', url, true); // true for asynchronous
+    xmlHttp.send(null);
+}
 
-    fetch('/light', {
+function changeLight(ip, colour) {
+    let data = {
+        text: colour,
+        ip: ip
+    };
+    fetch('http://24.212.130.181:8042/light', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data, ip)
     })
         .then((response) => response.json())
-        .then((data) => {
-            console.log('Success:', data);
-        })
         .catch((error) => {
             console.error('Error:', error);
         });
+}
+
+var url =
+    'https://ipgeolocation.abstractapi.com/v1/?api_key=35d5c77d03dd4152b8b9b251419cf2f6';
+
+function talk() {
+    httpGetAsync(url, 'green');
 }
 
 function khaana() {
-    const data = {
-        text: 'red'
-    };
-
-    fetch('/light', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    httpGetAsync(url, 'red');
 }
 
 function night() {
-    const data = {
-        text: 'blue'
-    };
-
-    fetch('/light', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    httpGetAsync(url, 'blue');
 }
